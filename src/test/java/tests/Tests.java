0dfca4b6bbfb6222e01f31d479a047f31b59c0d8
Thevakajan <thevakajan@gmail.com>
2019-06-27 21:08:48 +0530

    Automation-framework

---
+++ src/test/java/tests/Tests.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utils.TestBase;
+
+public class Tests extends TestBase {
+    @Test
+    public void testGenderlInformation(){
+
+    }
+}
